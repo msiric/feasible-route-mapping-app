a537970d3949258b76502a63c29bf7e0f5352443
msiric <mario.siric@mail.com>
2022-06-01 17:36:06 +0200

    Add swappable locations

--- client/src/forms/RouteForm/index.tsx
+++ client/src/forms/RouteForm/index.tsx
@@ -11,14 +11,16 @@ import {
   IconButton,
   List,
   ListItem,
+  ListItemAvatar,
   ListItemSecondaryAction,
-  Typography,
 } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import {
   Delete as DeleteIcon,
   AddLocationAlt as AddLocationIcon,
   HourglassEmpty as CalculateIcon,
+  ArrowDropUp as ArrowUp,
+  ArrowDropDown as ArrowDown,
 } from "@mui/icons-material";
 import { AutocompleteInput } from "@controls/Autocomplete";
 import { fetchAddress } from "@api/endpoints";
@@ -59,12 +61,25 @@ export const IsochroneForm = ({ handleFormSubmit }: IsochroneFormProps) => {
     formState: { isSubmitting, errors },
   } = useFormContext();
 
-  const { append, remove } = useFieldArray({
+  const { append, remove, swap } = useFieldArray({
     control,
     name: "options",
   });
 
   const values = watch();
+
+  const containsWaypoints = values.options.length > MINIMUM_NUMBER_OF_WAYPOINTS;
+
+  const handleLocationSwap = (origin: number, destination: number) => {
+    if (origin >= 0 && origin < values.options.length) {
+      if (destination >= 0 && destination < values.options.length) {
+        if (origin !== destination) {
+          swap(origin, destination);
+        }
+      }
+    }
+    return;
+  };
 
   return (
     <Box className={classes.container}>
@@ -79,73 +94,92 @@ export const IsochroneForm = ({ handleFormSubmit }: IsochroneFormProps) => {
               key={`${item.location?.lat}.${item.location?.lon}.${index}`}
               className={classes.listWrapper}
             >
-              <ListItem className={classes.listItem} disableGutters>
-                {index !== 0 && (
-                  <>
-                    <Divider
-                      className={classes.divider}
-                      orientation="vertical"
+              {index !== 0 && (
+                <Box className={classes.options}>
+                  <Divider className={classes.divider} orientation="vertical" />
+                  <Box className={classes.itemOptions}>
+                    <SelectInput
+                      {...register(`options.${index}.timeRange` as const, {
+                        required: true,
+                      })}
+                      control={control}
+                      label="Time range"
+                      options={TIME_RANGES}
+                      disabled={isSubmitting}
+                      error={!!errors.options?.[index]?.timeRange?.message}
+                      helperText={errors.options?.[index]?.timeRange?.message}
                     />
-                    <Box className={classes.itemOptions}>
-                      <SelectInput
-                        {...register(`options.${index}.timeRange` as const, {
-                          required: true,
-                        })}
-                        control={control}
-                        label="Time range"
-                        options={TIME_RANGES}
-                        disabled={isSubmitting}
-                        error={!!errors.options?.[index]?.timeRange?.message}
-                        helperText={errors.options?.[index]?.timeRange?.message}
-                      />
-                      <SelectInput
-                        {...register(
-                          `options.${index}.transportationMode` as const,
-                          { required: true }
-                        )}
-                        control={control}
-                        label="Transport mode"
-                        options={TRANSPORTATION_MODES}
-                        disabled={isSubmitting}
-                        error={
-                          !!errors.options?.[index]?.transportationMode?.message
-                        }
-                        helperText={
-                          errors.options?.[index]?.transportationMode?.message
-                        }
-                      />
-                    </Box>
-                    <Divider
-                      className={classes.divider}
-                      orientation="vertical"
+                    <SelectInput
+                      {...register(
+                        `options.${index}.transportationMode` as const,
+                        { required: true }
+                      )}
+                      control={control}
+                      label="Transport mode"
+                      options={TRANSPORTATION_MODES}
+                      disabled={isSubmitting}
+                      error={
+                        !!errors.options?.[index]?.transportationMode?.message
+                      }
+                      helperText={
+                        errors.options?.[index]?.transportationMode?.message
+                      }
                     />
-                  </>
-                )}
-                <AutocompleteInput
-                  {...register(`options.${index}.location` as const, {
-                    required: true,
-                  })}
-                  label={`Location ${index + 1}`}
-                  fetchData={fetchAddress}
-                  identifier="display_name"
-                  control={control}
-                  disabled={isSubmitting}
-                  error={
-                    !!(errors.options?.[index]?.location as FieldError)?.message
-                  }
-                  helperText={
-                    (errors.options?.[index]?.location as FieldError)
-                      ?.message ?? ""
-                  }
-                />
-                {values.options.length > MINIMUM_NUMBER_OF_WAYPOINTS && (
-                  <ListItemSecondaryAction className={classes.deleteIcon}>
+                  </Box>
+                  <Divider className={classes.divider} orientation="vertical" />
+                </Box>
+              )}
+              <ListItem
+                className={`${classes.listItem} ${
+                  containsWaypoints && classes.listItemPadding
+                }`}
+                disableGutters
+              >
+                <Box className={classes.controlContainer}>
+                  <ListItemAvatar className={classes.controlWrapper}>
                     <IconButton
+                      className={classes.swapIcon}
+                      aria-label="Move up"
+                      onClick={() => handleLocationSwap(index, index - 1)}
+                    >
+                      <ArrowUp />
+                    </IconButton>
+                    <IconButton
+                      className={classes.swapIcon}
+                      aria-label="Move down"
+                      onClick={() => handleLocationSwap(index, index + 1)}
+                    >
+                      <ArrowDown />
+                    </IconButton>
+                  </ListItemAvatar>
+                  <AutocompleteInput
+                    {...register(`options.${index}.location` as const, {
+                      required: true,
+                    })}
+                    label={`Location ${index + 1}`}
+                    fetchData={fetchAddress}
+                    identifier="display_name"
+                    control={control}
+                    disabled={isSubmitting}
+                    error={
+                      !!(errors.options?.[index]?.location as FieldError)
+                        ?.message
+                    }
+                    helperText={
+                      (errors.options?.[index]?.location as FieldError)
+                        ?.message ?? ""
+                    }
+                  />
+                </Box>
+                {containsWaypoints && (
+                  <ListItemSecondaryAction className={classes.deleteAction}>
+                    <IconButton
+                      className={classes.deleteIcon}
                       aria-label="Delete"
                       onClick={() => remove(index)}
                       disabled={isSubmitting}
                     >
-                      <DeleteIcon />
+                      <DeleteIcon fontSize="small" />
                     </IconButton>
                   </ListItemSecondaryAction>
                 )}
@@ -153,17 +187,19 @@ export const IsochroneForm = ({ handleFormSubmit }: IsochroneFormProps) => {
             </Box>
           ))}
           <Divider className={classes.divider} />
-          <AutocompleteInput
-            {...register(`excludeLocations` as const, {})}
-            multiple
-            label="Exclude locations"
-            fetchData={fetchAddress}
-            identifier="display_name"
-            control={control}
-            disabled={isSubmitting}
-            error={!!errors.excludeLocations?.message}
-            helperText={errors.excludeLocations?.message}
-          />
+          <Box className={classes.excludeLocations}>
+            <AutocompleteInput
+              {...register(`excludeLocations` as const, {})}
+              multiple
+              label="Exclude locations"
+              fetchData={fetchAddress}
+              identifier="display_name"
+              control={control}
+              disabled={isSubmitting}
+              error={!!errors.excludeLocations?.message}
+              helperText={errors.excludeLocations?.message}
+            />
+          </Box>
         </List>
         <Box className={classes.actions}>
           <Button
